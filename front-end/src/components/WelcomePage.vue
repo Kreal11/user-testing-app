e2f9bdecf064d4b6debe66ab956a31a0aed3e83e
Kreal11 <kirillshyrokov2@gmail.com>
2024-02-03 14:53:05 +0200

    added welcome and loginModal components

--- front-end/src/components/WelcomePage.vue
+++ front-end/src/components/WelcomePage.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>HelloWorld</div>
+  <div class="welcome-page">
+    <h1>Welcome on our User Assessment Application</h1>
+
+    <button @click="openLoginModal">Sign in</button>
+
+    <login-modal v-if="showLoginModal" @close="closeLoginModal" @login-success="onLoginSuccess" />
+  </div>
 </template>
 <script>
-export default {}
+import LoginModal from '@/components/LoginModal.vue'
+export default {
+  data() {
+    return {
+      showLoginModal: false
+    }
+  },
+  methods: {
+    openLoginModal() {
+      this.showLoginModal = true
+    },
+    closeLoginModal() {
+      this.showLoginModal = false
+    },
+    onLoginSuccess() {}
+  },
+  components: {
+    'login-modal': LoginModal
+  }
+}
 </script>
 <style scoped></style>
