b1d32628468f5db33fbad1d0853f49b04183f007
Kreal11 <kirillshyrokov2@gmail.com>
2024-02-03 18:23:58 +0200

    changed find params for authenticate

--- back-end/middlewares/authenticate.js
+++ back-end/middlewares/authenticate.js
@@ -1,8 +1,8 @@
 import { User } from '../model/user.js';
 
 export const authenticate = async (req, res, next) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email, password });
+  const { email } = req.body;
+  const user = await User.findOne({ email });
 
   if (!user) {
     return res.status(401).json({ message: 'Email or password is wrong' });
